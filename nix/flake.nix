a50756425be1a04e603a36a0198930a866d440cc
Matthew Toohey <contact@mtoohey.com>
2024-09-28 21:37:33 -0400

    add hyperfine to flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,7 +9,8 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      inherit (pkgs) bitwuzla boolector cvc5 mkShell python3 racket z3;
+      inherit (pkgs) bitwuzla boolector cvc5 hyperfine mkShell python3 racket
+        z3;
     in
     {
       # NOTE: `raco pkg install rosette` needs to be run manually.
@@ -18,6 +19,7 @@
           bitwuzla
           boolector
           cvc5
+          hyperfine
           (python3.withPackages (ps:
             let inherit (ps) matplotlib numpy; in [
               matplotlib
